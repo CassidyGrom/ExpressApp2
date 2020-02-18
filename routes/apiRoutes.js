cf02b1110be97eed00729ebd217f71aecf68282a
Cassidy Grom <cgrom@njadvancemedia.com>
2020-02-18 17:53:59 -0500

    front end working

---
+++ routes/apiRoutes.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const noteData = require("../db.json");
+
+router.get("/api/notes", (req, res) => {
+  res.json(noteData);
+});
+
+router.post("/api/notes", (req, res) => {
+  console.log(req.body);
+  noteData.push(req.body);
+  res.json(req.body);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,8 @@ const express = require("express");
 const noteData = require("./db.json");
 const path = require("path");
 const router = require("express").Router();
+const htmlRoutes = require("./routes/htmlRoutes");
+const apiRoutes = require("./routes/apiRoutes");
 
 //put express in a variable and name the port
 const app = express();
@@ -14,31 +16,13 @@ const PORT = 3011;
 //middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 //Get Requests HTML
 
-var potato = "POTATO?!?!?";
-console.log(potato);
-
-// app.get("*", (req, res) => {
-//   res.sendFile(path.join(__dirname, "index.html"));
-// });
-
-app.get("/notes", (req, res) => {
-  res.sendFile(path.join(__dirname, "./public/notes.html"));
-});
-
-//API get requests
-app.get("/api/notes", (req, res) => {
-  res.json(noteData);
-});
-
 //API post request
-app.post("/api/notes", (req, res) => {
-  console.log(req.body);
-  noteData.push(req.body);
-  res.json(req.body);
-});
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 //listen for the action
 app.listen(PORT, () => {
